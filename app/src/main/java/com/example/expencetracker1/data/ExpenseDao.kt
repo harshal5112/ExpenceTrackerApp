904d5cb027cf5ec44115a08dd07e9de55c4c5a53
harshal5112 <harshalrasoda@gmail.com>
2025-03-24 16:05:47 +0530

    Fixes

--- app/src/main/java/com/example/expencetracker1/data/ExpenseDao.kt
+++ app/src/main/java/com/example/expencetracker1/data/ExpenseDao.kt
@@ -3,7 +3,6 @@ package com.example.expencetracker1.data
 import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Delete
-import androidx.room.Insert
 import androidx.room.Query
 import androidx.room.Upsert
 
